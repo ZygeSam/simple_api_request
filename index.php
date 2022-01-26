875e4149e6cab773016d26ae8c388a279e6ec74d
ZygeSam <54424855+ZygeSam@users.noreply.github.com>
2022-01-26 01:15:06 +0100

    Not done Refactoring http request

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+// $curl =  curl_init();
+// curl_setopt($curl, CURLOPT_URL, 'http://127.0.0.1:8000/api/posts');
+// curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+// $response = curl_exec($curl);
+// $data = json_decode($response,true);
+
+// curl_setopt($curl, CURLOPT_POST, true);
+// curl_setopt($curl, CURLOPT_POSTFIELDS, [
+//                'page_id' => '1',
+//                'title'=> 'A Woman\'s Scorned',
+//                'content' => 'Trilogy of Aduke',
+//            ]);
+// $response = curl_exec($curl);
+//	curl_close
+// return $response;
+
 
  function consume($url, $method='GET', $arr=[], $type=['Content-Type'=>'application/json']){
 	$curl = curl_init();
@@ -15,4 +31,12 @@
 	$response = json_decode($response);
 	return $response;
 	}
+	$arry = [
+               'page_id' => '2',
+               'title'=> 'Men\'s Wisdom',
+               'content' => 'The Blindspot of players',
+           ];
+	// consume('http://127.0.0.1:8000/api/posts', 'POST', $arry, 'application/json');
+
+           consume('http://127.0.0.1:8000/api/posts');
 ?>
